f7194aa2f22223fa529264ac36983179dd8d39e1
Sergio Tojo <sergiotojo.unlz@gmail.com>
2024-04-22 19:01:44 -0300

    Ejercicios de conteo a 0 sea mayor o menor a este

--- EjerciciosC - TP - Clase 4/conteoA0PosYNeg.cpp
+++ EjerciciosC - TP - Clase 4/conteoA0PosYNeg.cpp
@@ -3,23 +3,24 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
 
-int inicio = 0;
-int contador = 1;
+    int inicio = 0; //Declaración de valores
+    int contador = 1; //Declaración de valores
 
-cout<<"Ingrese un valor inicial"<<endl;
-cin>>inicio;
-while(inicio>0){
-cout<<"Valor inicial "<<contador<<": "<<inicio<<endl;
-inicio--;
-contador++;
-}while (inicio<0){
-    cout<<"Valor inicial "<<contador<<": "<<inicio<<endl;
-    inicio++;
-    contador++;
-}
-
-
-
+    cout << "Ingrese un valor inicial" << endl; //Solicito por pantalla el ingreso de un valor
+    cin >> inicio; //Ingreso por teclado del valor
+    while (inicio > 0) //Mientras el valor "inicio" sea mayor a 0 ingresa a este bucle
+    {
+        cout << "Valor inicial " << contador << ": " << inicio << endl; //Se muestra el conteo + el valor inicial
+        inicio--; //Se resta el valor inicial hasta llegar a 0
+        contador++; //Se suma el contador para mostrar la cantidad de impresiones hasta el 0
+    }
+    while (inicio < 0)//Mientras el valor "inicio" sea menor a 0 ingresa a este bucle
+    {
+        cout << "Valor inicial " << contador << ": " << inicio << endl; //Se muestra el conteo + el valor inicial
+        inicio++; //Se suma el valor inicial hasta llegar a 0
+        contador++; //Se resta el contador para mostrar la cantidad de impresiones hasta el 0
+    }
 }
