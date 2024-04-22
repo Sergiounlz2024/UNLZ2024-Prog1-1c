8c0e3282425f96565e1161f904f2bf8c872e5685
Sergio Tojo <sergiotojo.unlz@gmail.com>
2024-04-22 19:33:53 -0300

    Último ejercicio: positivos

--- EjerciciosC - TP - Clase 4/positivos.cpp
+++ EjerciciosC - TP - Clase 4/positivos.cpp
@@ -7,19 +7,19 @@ int main()
 {
     // Crear un programa donde los valores sean siempre positivos.
 
-    int valor = 0; //Declaro variables necesarias
-    int suma = 0; //Declaro variables necesarias
-    int contador = 0; //Declaro de variables necesarias
-    while (valor >= 0) //Mientras el valor sea igual o mayor a 0
+    int valor = 0;     // Declaro variables necesarias
+    int suma = 0;      // Declaro variables necesarias
+    int contador = 0;  // Declaro de variables necesarias
+    while (valor >= 0) // Mientras el valor sea igual o mayor a 0
     {
-        cout << "Ingrese un valor"<<endl; //Solicito ingresar el valor en pantalla
-        cin >> valor; //Ingreso de valor por teclado
-        if (valor >= 0) //Si el valor es >= 0 va a sumar y acumular el valor en la variable "suma"
+        cout << "Ingrese un valor" << endl; // Solicito ingresar el valor en pantalla
+        cin >> valor;                       // Ingreso de valor por teclado
+        if (valor >= 0)                     // Si el valor es >= 0 va a sumar y acumular el valor en la variable "suma"
         {
-            suma = suma + valor;
-            contador++;
+            suma = suma + valor; //Acumulamos la suma en la variable "suma"
+            contador++; //Incrementamos el contador
         }
     }
-    cout << "Suma total: " << suma<<endl; //De lo contrario, únicamente va a mostrar el valor final de la suma.
-    cout << "Se ingresaron "<<contador<<" numeros antes de ingresar un valor negativo"<<endl; //Muestro la cantidad de valores que se ingresaron previo al valor negativo
+    cout << "Suma total: " << suma << endl;                                                         // De lo contrario, únicamente va a mostrar el valor final de la suma.
+    cout << "Se ingresaron " << contador << " numeros antes de ingresar un valor negativo" << endl; // Muestro la cantidad de valores que se ingresaron previo al valor negativo
 }
